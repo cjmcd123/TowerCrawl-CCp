2181206d0f416193943ea6e958bfe7bbdcb6f901
Jack Murning <jackrichardmurning@gmail.com>
2018-06-28 10:31:30 +0100

    finish abstract items

--- src/main/java/Items/Item.java
+++ src/main/java/Items/Item.java
@@ -3,4 +3,23 @@ package Items;
 public abstract class Item {
     private String name;
     private int hp_modifier;
+    private boolean causesDamage;
+
+    public Item(String name, int hp_modifier, boolean causesDamage) {
+        this.name = name;
+        this.hp_modifier = hp_modifier;
+        this.causesDamage = causesDamage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp_modifier() {
+        return hp_modifier;
+    }
+
+    public boolean isCausesDamage() {
+        return causesDamage;
+    }
 }
